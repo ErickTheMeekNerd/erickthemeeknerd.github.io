17c9288fe7575b307e154277888b6b612c62b2b6
Erick Martinez <erickmichealmartinez@gmail.com>
2018-05-16 01:12:25 -0700

    changing my website

--- story.js
+++ story.js
@@ -1,4 +1,4 @@
-"There once was a [adjective] programmer who wanted to use Javascript to [verb] the [noun]."
+"There once was a [adjective] programmer who wanted to use Javascript to [verb] the [noun]."  
 
 var verb = prompt("What's your favorite thing to do?");
 var noun = prompt("Who is a favorite person of yours? Do not include anything but the name of that person");
