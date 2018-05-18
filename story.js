f6b770d7ae658ee007e57031685a4a1903e6b230
Erick Martinez <erickmichealmartinez@gmail.com>
2018-05-18 00:32:16 -0700

    added js

--- story.js
+++ story.js
@@ -1,12 +1,18 @@
-"There once was a [adjective] programmer who wanted to use Javascript to [verb] the [noun]."  
+// "There once was a [adjective] programmer who wanted to use Javascript to [verb] the [noun]."
+//
+// var verb = prompt("What's your favorite thing to do?");
+// var noun = prompt("Who is a favorite person of yours? Do not include anything but the name of that person");
+// var adjective = prompt("Please type an adjective");
+//
+// var sentence = "There once was a " + adjective;
+//
+// alert("All done. Ready for the messsage?");
+// sentence += " programmer who wanted to use Javascript to " + verb;
+// sentence += " with " + noun + ".";
+// alert(sentence);
 
-var verb = prompt("What's your favorite thing to do?");
-var noun = prompt("Who is a favorite person of yours? Do not include anything but the name of that person");
-var adjective = prompt("Please type an adjective");
-
-var sentence = "There once was a " + adjective;
-
-alert("All done. Ready for the messsage?");
-sentence += " programmer who wanted to use Javascript to " + verb;
-sentence += " with " + noun + ".";
-alert(sentence);
+var age = prompt("How old are you?");
+var deathAge = prompt("What age do you think you will live too?");
+var yearsToLive = parseInt(deathAge) - parseInt(age);
+alert("WOW! You have " + yearsToLive + " years to live!");
+console.log();
